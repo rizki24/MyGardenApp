66773f38fc09c56cc8c0c577d0d4937249fab273
RyanBekti <ryanbekti86@gmail.com>
2018-01-01 20:21:58 +0700

    Starting Code

--- app/src/test/java/id/sch/smktelkom_mlg/learn/xiirpl128/mygarden/ExampleUnitTest.java
+++ app/src/test/java/id/sch/smktelkom_mlg/learn/xiirpl128/mygarden/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package id.sch.smktelkom_mlg.learn.xiirpl128.mygarden;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static junit.framework.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
